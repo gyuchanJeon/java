6575de632976f439a6ef545530514cf28bee3186
gyuchan <ian810900@gmail.com>
2024-07-05 20:52:23 +0900

    add

--- BackJun/src/step3/Test07.java
+++ BackJun/src/step3/Test07.java
@@ -4,17 +4,16 @@ import java.util.Scanner;
 
 public class Test07 {
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int T = sc.nextInt();
-		
+
 		for (int i = 1; i <= T; i++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
-			System.out.println("Case #"+i+": " + (a+b));
+			System.out.println("Case #" + i + ": " + (a + b));
 		}
-		
-		
+		sc.close();
 	}
 }
